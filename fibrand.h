e3fc1161646f7a6b587371e7823ab79add78df1f
rfr111 <rafa.franca21@hotmail.com>
2011-05-18 20:22:50 -0300

    Adding fibrand.h

---
+++ fibrand.h
@@ -0,0 +1,18 @@
+#ifndef FIBRAND
+#define FIBRAND
+#include <math.h>
+int fibrand(int nseed1, int nseed2, int ntimes) {
+    int nfib = 0;
+    int nfib1 = nseed1;
+    int nfib2 = nseed2;
+
+    for (int iii = 0; iii < ntimes; iii++)
+    {
+        nfib = nfib1 + nfib2;
+        nfib1 = nfib2;
+        nfib2 = nfib;
+    }
+
+    return nfib;
+}
+#endif
